172689dce488a85e48f6a1c4dc819aa19418e5ce
Priyank Jain <priyankjlns@gmail.com>
2014-06-27 00:59:46 +0530

    Limiting Max URL to 1000

--- Sitemap.php
+++ Sitemap.php
@@ -30,7 +30,7 @@ class Sitemap {
 	const EXT = '.xml';
 	const SCHEMA = 'http://www.sitemaps.org/schemas/sitemap/0.9';
 	const DEFAULT_PRIORITY = 0.5;
-	const ITEM_PER_SITEMAP = 50000;
+	const ITEM_PER_SITEMAP = 1000;
 	const SEPERATOR = '-';
 	const INDEX_SUFFIX = 'index';
 
